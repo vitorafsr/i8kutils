960f7cdcef4f0996adc68de7c79232a1af00f229
Vitor Augusto <vitorafsr@gmail.com>
2014-12-03 11:40:43 -0200

    Include i8kctl.h.

---
+++ i8kctl.h
@@ -0,0 +1,14 @@
+int i8k_get_bios_version();
+int i8k_get_machine_id();
+int i8k_get_cpu_temp();
+int i8k_get_fan_status(int);
+int i8k_get_fan_status(int);
+int i8k_get_fan_speed(int);
+int i8k_get_fan_speed(int);
+int i8k_get_power_status();
+int i8k_get_fn_status();
+
+#ifdef LIB
+void init();
+void finish();
+#endif
